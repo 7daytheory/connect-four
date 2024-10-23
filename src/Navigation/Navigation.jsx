f8790fa2364d33d7afe95e6fc1c54c0560e427c7
Matthew Lowe <matthewjlowe11@gmail.com>
2024-10-23 15:02:14 -0400

    Create basic nav, import to App

---
+++ src/Navigation/Navigation.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Logo from '../assets/logo.png'
+
+const Navigation = () => {
+  return (
+    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="https://matthewjlowe.com" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src={Logo} className="h-8" alt="Connect Four Logo" />
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Connect Four</span>
+            </a>
+        </div>
+    </nav>
+  )
+}
+
+export default Navigation
