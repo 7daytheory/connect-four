6cc6a3f9f390649adb4954eeee4a0791eba8b504
Matthew Lowe <matthewjlowe11@gmail.com>
2024-10-23 15:06:18 -0400

    Installed tailwindcss, updated classNames

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -6,7 +6,7 @@ const Navigation = () => {
     <nav class="bg-white border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="https://matthewjlowe.com" className="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src={Logo} className="h-8" alt="Connect Four Logo" />
+                <img src={Logo} className="h-12" alt="Connect Four Logo" />
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Connect Four</span>
             </a>
         </div>
