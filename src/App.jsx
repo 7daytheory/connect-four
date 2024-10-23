70c7ade5de9c6452203c3c097b4d28d48cea9a0a
Matthew Lowe <matthewjlowe11@gmail.com>
2024-10-23 15:23:22 -0400

    Created a circle icon, pass in the color with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Circle from "./Circle/Circle"
 import Navigation from "./Navigation/Navigation"
 
 function App() {
   return (
     <>
       <Navigation />
+      <Circle color={'bg-blue-600'}/>
+      <Circle color={'bg-red-600'}/>
     </>
   )
 }
