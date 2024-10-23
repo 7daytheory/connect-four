6f8eee1bfd1cad4cd4b9ffac8a59c4d22ad965b7
Matthew Lowe <matthewjlowe11@gmail.com>
2024-10-23 15:24:41 -0400

    Changed class to className

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -3,11 +3,11 @@ import Logo from '../assets/logo.png'
 
 const Navigation = () => {
   return (
-    <nav class="bg-white border-gray-200 dark:bg-gray-900">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="https://matthewjlowe.com" className="flex items-center space-x-3 rtl:space-x-reverse">
                 <img src={Logo} className="h-12" alt="Connect Four Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Connect Four</span>
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Connect Four</span>
             </a>
         </div>
     </nav>
